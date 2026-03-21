9a16bf07a17adc650ba789c208ddfec5f32cdfe3
Shaun Tran <sontl@mac.com>
2026-03-21 22:38:36 +0700

    add allowedHosts to o.ai.vn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['techviflaw.vn.gd'],
+    allowedHosts: ['techviflaw.o.ai.vn', 'techviflaw.vn.gd'],
   },
 })
